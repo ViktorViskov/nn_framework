759f86844ba5042b660d6226681ddf806879c534
Viktor Viskov <carrergt@gmail.com>
2021-08-31 08:52:02 +0200

    Version with 2 hidden 1 input and 2 output

--- core/network.py
+++ core/network.py
@@ -8,7 +8,7 @@ class NN:
     def __init__(self):
 
         # learn rate
-        learn_rate = 0.1
+        learn_rate = 0.3
 
         # Creating neurons
 
@@ -26,9 +26,7 @@ class NN:
 
         # output
         self.output1 = Neuron(learn_rate,np.random.random(),np.random.random())
-        self.output2 = Neuron(learn_rate,np.random.random(),np.random.random(),np.random.random())
-        self.output3 = Neuron(learn_rate,np.random.random(),np.random.random(),np.random.random())
-        self.output4 = Neuron(learn_rate,np.random.random(),np.random.random())
+        self.output2 = Neuron(learn_rate,np.random.random(),np.random.random())
 
     # Method for prediction
     def Predict(self, data):
@@ -47,12 +45,10 @@ class NN:
 
         # output
         r_o1 = self.output1.Predict(np.array([r_l2_h1, r_l2_h2]))
-        r_o2 = self.output2.Predict(np.array([r_l2_h1, r_l2_h2, r_l1_h3]))
-        r_o3 = self.output3.Predict(np.array([r_l2_h2, r_l2_h3, r_l2_h4]))
-        r_o4 = self.output4.Predict(np.array([r_l2_h3, r_l2_h4]))
+        r_o2 = self.output2.Predict(np.array([r_l2_h3, r_l2_h4]))
 
         # output
-        return np.array([r_o1,r_o2,r_o3,r_o4])
+        return np.array([r_o1,r_o2])
     
     # Method for one time training
     def Train(self, data, answer, epoch):
@@ -64,16 +60,14 @@ class NN:
         # output
         self.output1.Calibrate_Weights(answer[0])
         self.output2.Calibrate_Weights(answer[1])
-        self.output3.Calibrate_Weights(answer[2])
-        self.output4.Calibrate_Weights(answer[3])
 
         # layer 2
-        self.laeyr2_hidden1.Back_Propagation_Calibrate_Weigth((self.output1.back_propagation_error_sizes[0] + self.output2.back_propagation_error_sizes[0]) / 2)
-        self.laeyr2_hidden2.Back_Propagation_Calibrate_Weigth((self.output1.back_propagation_error_sizes[1] + self.output2.back_propagation_error_sizes[1] + self.output3.back_propagation_error_sizes[0]) / 3)
-        self.laeyr2_hidden3.Back_Propagation_Calibrate_Weigth((self.output2.back_propagation_error_sizes[2] + self.output3.back_propagation_error_sizes[1] + self.output4.back_propagation_error_sizes[0]) / 3)
-        self.laeyr2_hidden4.Back_Propagation_Calibrate_Weigth((self.output3.back_propagation_error_sizes[2] + self.output4.back_propagation_error_sizes[1]) / 2)
+        self.laeyr2_hidden1.Back_Propagation_Calibrate_Weigth(self.output1.back_propagation_error_sizes[0])
+        self.laeyr2_hidden2.Back_Propagation_Calibrate_Weigth(self.output1.back_propagation_error_sizes[1])
+        self.laeyr2_hidden3.Back_Propagation_Calibrate_Weigth(self.output2.back_propagation_error_sizes[0])
+        self.laeyr2_hidden4.Back_Propagation_Calibrate_Weigth(self.output2.back_propagation_error_sizes[1])
 
-        # layer 2
+        # layer 1
         self.laeyr1_hidden1.Back_Propagation_Calibrate_Weigth((self.laeyr2_hidden1.back_propagation_error_sizes[0] + self.laeyr2_hidden2.back_propagation_error_sizes[0]) / 2)
         self.laeyr1_hidden2.Back_Propagation_Calibrate_Weigth((self.laeyr2_hidden1.back_propagation_error_sizes[1] + self.laeyr2_hidden2.back_propagation_error_sizes[1] + self.laeyr2_hidden3.back_propagation_error_sizes[0]) / 3)
         self.laeyr1_hidden3.Back_Propagation_Calibrate_Weigth((self.laeyr2_hidden2.back_propagation_error_sizes[2] + self.laeyr2_hidden3.back_propagation_error_sizes[1] + self.laeyr2_hidden4.back_propagation_error_sizes[0]) / 3)
@@ -83,8 +77,6 @@ class NN:
         print(epoch)
         print("1 Error size %f" % ( np.mean((output_result[0] - answer[0]) ** 2)))
         print("2 Error size %f" % ( np.mean((output_result[1] - answer[1]) ** 2)))
-        print("3 Error size %f" % ( np.mean((output_result[2] - answer[2]) ** 2)))
-        print("4 Error size %f" % ( np.mean((output_result[3] - answer[3]) ** 2)))
 
     # Method for Learn network
     def Learn(self, train_array, epoch = 5000):
@@ -95,7 +87,7 @@ class NN:
 
                 # preprocess data
                 train_data = np.array([data[0]])
-                answer = np.array([data[1],data[2],data[3],data[4]])
+                answer = np.array([data[1],data[2]])
 
                 # learn
                 self.Train(train_data, answer, i)
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from core.network import NN
 
 # data for check
 train_array = np.array([
-    [1,1,0,0,0],
-    [5,0,1,0,0],
-    [10,0,0,1,0],
-    [15,0,0,0,1],
+    [-35,1,0],
+    [-25,1,0],
+    [-15,1,0],
+    [0,1,0],
+    [5,1,0],
+    [35,0,1],
+    [45,0,1],
+    [50,0,1],
     ])
 
 brain = NN()
 
-brain.Learn(train_array, 50000)
+brain.Learn(train_array, 5000)
 
 
 while True:
